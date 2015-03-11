e54949d2c771ead8d3d61fb89672a184e55a06c6
Dominique Busser <dombusser@gmail.com>
2015-03-11 15:30:46 +0100

    added second test

--- spec/MyCtrlSpec.js
+++ spec/MyCtrlSpec.js
@@ -11,6 +11,13 @@ describe('MyCtrlSpec', function() {
 		})
 	}))
 
+	it('should call add', function() {
+		spyOn(scope, 'add');
+		scope.add(3, 4);
+		expect(scope.add).toHaveBeenCalledWith(3, 4);
+	});
+
+
 	it('should add to numbers', function() {
 		expect(scope.add(1, 2)).toEqual(3);
 	})
